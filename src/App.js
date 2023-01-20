f259778c910de1b8ea2a3038c3addbaaf33e5364
quan <lehoangquan@gmail.com>
2023-01-20 14:48:18 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import HomePage from "./pages/Home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DetailProductPage from "./pages/DetailProduct/DetailProductPage";
+import NavBar from "./components/Navbar/NavBar";
+import Cart from "./components/Cart/Cart";
+import UserOrders from "./components/User/UserOrder/UserOrder";
+import UserProfiles from "./components/User/UserOrder/UserProfile";
+import UserFavoriteComponent from "./components/User/UserFavorate/UserFavorate";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<NavBar />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/detailPage/:id" element={<DetailProductPage />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/user/order" element={<UserOrders />} />
+            <Route path="/user/profile" element={<UserProfiles />} />
+            <Route path="/user/favorite" element={<UserFavoriteComponent />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
