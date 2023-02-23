9873cf1b5c7f0b60cc5825aa50af965d98960407
quan <lehoangquan@gmail.com>
2023-02-23 21:49:19 +0700

    @

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
-import { applyMiddleware, compose, createStore } from 'redux';
-import rootReducer from './redux/reducer/reducer';
+import React from "react";
+import { ReactDOM } from "react";
+
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { applyMiddleware, compose, createStore } from "redux";
+import rootReducer from "./redux/reducer/reducer";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-//  applymiddleware để dành giải thích sau 
+//  applymiddleware để dành giải thích sau
 const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(thunk))
-)
-const root = ReactDOM.createRoot(document.getElementById('root'));
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
